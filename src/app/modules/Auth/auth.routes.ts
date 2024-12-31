167ea80248d085c5997bc2c6fe89154e8d9d6ebb
Tareq <razzbabut@gmail.com>
2025-01-01 00:25:45 +0600

    implement catchAsync utils & validateRequest middleware

--- src/app/modules/Auth/auth.routes.ts
+++ src/app/modules/Auth/auth.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { AuthControllers } from "./auth.controller";
+import validateRequest from "../middleware/validateRequest";
+import { UserValidation } from "../User/user.validation";
 
 const router = Router();
 
-router.post('/register', AuthControllers.userRegister);
+router.post('/register', validateRequest(UserValidation.userValidationSchema), AuthControllers.userRegister);
 
 export const AuthRouter = router;
